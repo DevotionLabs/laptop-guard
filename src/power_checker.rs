97e16a0a0c3fd83a417d7bf12aa68859544014bc
diego <diego.undevfined@gmail.com>
2025-02-21 19:55:19 +0100

    Remove unnecessary battery loop

--- src/power_checker.rs
+++ src/power_checker.rs
@@ -4,9 +4,10 @@ pub fn is_plugged() -> bool {
     let manager = Manager::new().expect("Failed to initialize battery manager");
 
     if let Ok(batteries) = manager.batteries() {
-        for battery in batteries.flatten() {
+        if let Some(battery) = batteries.flatten().next() {
             return battery.state() == State::Charging;
         }
     }
+
     false
 }
