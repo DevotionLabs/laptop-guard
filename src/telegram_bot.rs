adb1ff4d42f4c9871f7aa202ed5124a7489b65ab
diego <diego.undevfined@gmail.com>
2025-02-21 18:37:24 +0100

    Handle user requests on TG bot

--- src/telegram_bot.rs
+++ src/telegram_bot.rs
@@ -1,5 +1,5 @@
-use crate::logger::info;
-use teloxide::prelude::*;
+use crate::logger::{error, info};
+use teloxide::{prelude::*, repl, types::ParseMode, utils::markdown::escape};
 use tokio::signal;
 
 pub struct TelegramBot {
@@ -9,17 +9,58 @@ pub struct TelegramBot {
 impl TelegramBot {
     pub fn new(token: String) -> Self {
         Self {
-            bot: Bot::new(token),
+            bot: Bot::new(&token),
         }
     }
 
     pub async fn run(&self) {
-        info("🤖 Telegram bot is online!");
+        repl(self.bot.clone(), |bot: Bot, msg: Message| async move {
+            Self::handle_message(bot, msg).await;
+            Ok(())
+        })
+        .await;
 
         signal::ctrl_c()
             .await
             .expect("Failed to listen for shutdown signal");
 
         info("🛑 Telegram bot shutting down...");
+    }
+
+    async fn handle_message(bot: Bot, msg: Message) {
+        match msg.text() {
+            Some("/chatid") => Self::handle_chatid(&bot, msg).await,
+            _ => Self::handle_default(&bot, msg).await,
+        }
+    }
+
+    async fn handle_chatid(bot: &Bot, msg: Message) {
+        let chat_id = msg.chat.id;
+
+        let reply_msg = format!("Your chat ID is: `{}`", chat_id);
+
+        Self::send_markdown_message(bot, chat_id, &reply_msg).await;
+    }
+
+    async fn handle_default(bot: &Bot, msg: Message) {
+        let helper_text = "🔔 Laptop Guard Alert System\n\
+        This bot alerts you in this chat if your laptop is unplugged while Laptop Guard is running.\n\n\
+        📌 Available commands:\n\
+        /chatid -> Retrieve your chat ID (needed when running the binary).\n\n\
+        ℹ️ No other commands are available. Just wait for alerts if your laptop is unplugged.";
+
+        Self::send_markdown_message(bot, msg.chat.id, helper_text).await;
+    }
+
+    async fn send_markdown_message(bot: &Bot, chat_id: ChatId, msg: &str) {
+        let escaped_msg = escape(msg);
+
+        if let Err(e) = bot
+            .send_message(chat_id, escaped_msg)
+            .parse_mode(ParseMode::MarkdownV2)
+            .await
+        {
+            error(&format!("Failed to send Telegram message: {}", e));
+        }
     }
 }
