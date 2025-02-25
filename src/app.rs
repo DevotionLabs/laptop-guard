334df67d11fa3e2c64c1210e51be817af4d3e87c
diego <diego.undevfined@gmail.com>
2025-02-25 14:10:36 +0100

    Split initialization tasks

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,6 @@
 use crate::{
     app_config::AppConfig, logger::info, service_factory::ServiceFactory,
-    task_manager::TaskManager, validator::validate_tg_bot_token,
+    task_manager::TaskManager, telegram_bot::TelegramBot, validator::validate_tg_bot_token,
 };
 use tokio::task;
 
@@ -16,7 +16,10 @@ impl App {
     pub async fn run(&self) -> Result<(), Box<dyn std::error::Error>> {
         validate_tg_bot_token(&self.config.bot_token)?;
 
-        let (bot_task, guard_task) = self.initialize_services();
+        let bot = ServiceFactory::create_bot(&self.config.bot_token);
+
+        let bot_task = self.initialize_bot(bot.clone());
+        let guard_task = self.initialize_guard(bot);
 
         let mut task_manager = TaskManager::new(bot_task, guard_task);
         task_manager.run().await;
@@ -24,24 +27,26 @@ impl App {
         Ok(())
     }
 
-    fn initialize_services(&self) -> (task::JoinHandle<()>, Option<task::JoinHandle<()>>) {
-        let bot = ServiceFactory::create_bot(&self.config.bot_token);
-        let bot_task = {
-            let bot_clone = bot.clone();
-            task::spawn(async move { bot_clone.run().await })
+    fn initialize_bot(&self, bot: TelegramBot) -> task::JoinHandle<()> {
+        info("Starting telegram bot...");
+        info("Use /chatid command to get your chat ID");
+
+        let bot_clone = bot.clone();
+        task::spawn(async move { bot_clone.run().await })
+    }
+
+    fn initialize_guard(&self, bot: TelegramBot) -> Option<task::JoinHandle<()>> {
+        let chat_id = match self.config.chat_id {
+            Some(id) => id,
+            None => return None,
         };
 
-        if let Some(chat_id) = self.config.chat_id {
-            info("Starting laptop guard process in full mode...");
-            info(&format!("Chat ID: {}", chat_id));
+        info("Starting laptop guard process in full mode...");
+        info(&format!("Chat ID: {}", chat_id));
 
-            let guard = ServiceFactory::create_guard(bot, chat_id, self.config.interval);
-            let guard_task = task::spawn_blocking(move || guard.start());
-            (bot_task, Some(guard_task))
-        } else {
-            info("Starting laptop guard in bot-only mode...");
-            info("Use /chatid command to get your chat ID");
-            (bot_task, None)
-        }
+        let guard = ServiceFactory::create_guard(bot, chat_id, self.config.interval);
+        let guard_task = task::spawn_blocking(move || guard.start());
+
+        Some(guard_task)
     }
 }
